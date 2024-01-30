b7ce5bbf83b1219a6101fb1db674e492c85d3990
bpeters3 <97409612+bpeters3@users.noreply.github.com>
2024-01-29 21:57:08 -0500

    Added anti-aliasing
    - Added random double generator utility fcns (rtweekend.h)
    - Added camera pixel samples option (camera.h)
    - Added clamp to inverval (interval.h)
    -Modified main to support new settings

--- interval.h
+++ interval.h
@@ -19,7 +19,11 @@ class interval {
 
       static const interval empty,universe;
       
-      
+      double clamp(double x) const{
+        if(x<min) return min;
+        if(x>max) return max;
+        return x;
+      }
       
 };
 
